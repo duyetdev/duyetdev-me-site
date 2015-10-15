841b967aae0b59c31bb51a0ed6c0a26d13580963
Van-Duyet Le <me@duyetdev.com>
2015-10-15 22:49:34 +0700

    Move all assets to assets folder

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+/*-------------------------------------------------*/
+/* =  Full-window section
+/*-------------------------------------------------*/
+
+var windowHeight = $(window).height(),
+topSection = $('.home-fullscreen');
+topSection.css('height', windowHeight);
+
+$(window).resize(function(){
+var windowHeight = $(window).height();
+topSection.css('height', windowHeight);
+});
+/* ==============================================
+Preloader
+=============================================== */
+
+$(window).load(function() {
+    $('.status').fadeOut();
+    $('.preloader').delay(350).fadeOut('slow');
+});
