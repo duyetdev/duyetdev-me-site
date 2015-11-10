99bad7096884fe08e715c31e0b448547a7db0191
Van-Duyet Le <lvduit08@gmail.com>
2015-11-11 01:28:32 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,21 +53,24 @@ function render_project_datatable(table_id, github) {
 function render_project_datatable_1(github) {
 	return render_project_datatable('#github_1', github);
 }
-function render_project_datatable_1(github) {
-	return render_project_datatable('#github_1', github);
+function render_project_datatable_2(github) {
+	return render_project_datatable('#github_2', github);
 }
-function render_project_datatable_1(github) {
-	return render_project_datatable('#github_1', github);
+function render_project_datatable_3(github) {
+	return render_project_datatable('#github_3', github);
 }
-function render_project_datatable_1(github) {
-	return render_project_datatable('#github_1', github);
+function render_project_datatable_4(github) {
+	return render_project_datatable('#github_4', github);
 }
-function render_project_datatable_1(github) {
-	return render_project_datatable('#github_1', github);
+function render_project_datatable_5(github) {
+	return render_project_datatable('#github_5', github);
 }
-function render_project_datatable_1(github) {
-	return render_project_datatable('#github_1', github);
+function render_project_datatable_6(github) {
+	return render_project_datatable('#github_6', github);
 }
-function render_project_datatable_1(github) {
-	return render_project_datatable('#github_1', github);
+function render_project_datatable_7(github) {
+	return render_project_datatable('#github_7', github);
+}
+function render_project_datatable_8(github) {
+	return render_project_datatable('#github_8', github);
 }
